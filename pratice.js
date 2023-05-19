2abb649fbf9e676db03c05b4ded72c0353fc8485
sajib689 <jibonraja11@gmail.com>
2023-05-19 14:18:08 +0600

    apple modal completed

--- pratice.js
+++ pratice.js
@@ -34,7 +34,8 @@ const displayPhone = (phones, dataLimit) => {
         <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
           <p class="card-text">${phone.slug}</p>
-          <div id="btn-show-all" class="btn btn-primary" onclick="showDetails('${phone.slug}')">Show Detail</div>
+          <div id="btn-show-all" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="showDetails('${phone.slug}')">Show Detail</div>
+      
         </div>
       </div>
         `
@@ -72,6 +73,28 @@ document.getElementById('btn-show-all').addEventListener('click', function() {
 const showDetails = id => {
   fetch(` https://openapi.programming-hero.com/api/phone/${id}`)
   .then(res => res.json())
-  .then(data => console.log(data.data))
+  .then(data => displayModal(data.data))
 }
-loadPhone('')
+
+const displayModal = modal => {
+  console.log(modal)
+      const modalTitle = document.getElementById('exampleModalLabel')
+      modalTitle.innerText = `${modal.name}`
+      const phoneFeatures = document.getElementById('phoneFeature')
+      phoneFeatures.innerHTML = `
+      <img  src="${modal.image}"/>
+      <p>ChipSet: ${modal.mainFeatures.chipSet}</p>
+      <p>Display Size: ${modal.mainFeatures.displaySize}</p>
+      <p>Memory: ${modal.mainFeatures.memory}</p>
+      
+      <p>Storage: ${modal.mainFeatures.storage}</p>
+      <p>Others: ${modal.others.Bluetooth}</p>
+     
+      
+      `
+      const modalDetails = document.getElementById('phone-details')
+      modalDetails.innerHTML = `
+      <p>Release Date: ${modal.releaseDate}</p>
+      `
+}
+loadPhone('apple')
